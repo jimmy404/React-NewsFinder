b2720ffab9185c21eb99d87fdd045cf514dff1c7
Jimmy200 <bruno_pier@hotmail.com>
2021-04-27 00:14:15 -0300

    Finishes configuring the custom hook by adding the options to select

--- src/hooks/useSelect.js
+++ src/hooks/useSelect.js
@@ -2,11 +2,17 @@ import React, {useState} from 'react';
 
 const useSelect = (initialState, options) => {
 
-  const [state, setState] = useState('');
+  const [state, setState] = useState(initialState);
 
   const SelectNews = () => (
-    <select className="browser-default">
-      <option value="">Select</option>
+    <select
+      className="browser-default"
+      value={state}
+      onChange={e => setState(e.target.value)}
+    >
+      {options.map(option => (
+        <option key={option.value} value={option.value}>{option.label}</option>
+      ))}
     </select>
   );
 
