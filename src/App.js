b5f0e6cc2cf565b1f1797eb10784afd5a07a7bbe
Jimmy200 <bruno_pier@hotmail.com>
2021-04-27 00:30:37 -0300

    Querying the API and reading the results

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React, {Fragment, useState} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
 import Form from './components/Form';
 import Header from './components/Header';
 
 function App() {
 
   const [category, saveCategory] = useState('');
+  const [news, saveNews] = useState([]);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=5bdc8077784142db9ac390ab5b8fd753`;
+      const response = await fetch(url);
+      const news = await response.json();
+      saveNews(news.articles);
+    }
+    fetchAPI();
+  }, [category]);
 
   return (
     <Fragment>
